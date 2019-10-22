9852aa5c7fe680939d72eba1548eee07ba58807b
Elivelton Santos <eliveltonoliveira2009@hotmail.com>
2019-10-22 11:23:57 -0300

    Pedido, Pagamento, EstadoParamento

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -37,6 +37,9 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 	}
 
@@ -105,6 +108,14 @@ public class Cliente implements Serializable {
 		this.email = email;
 	}
 	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -130,8 +141,5 @@ public class Cliente implements Serializable {
 		return true;
 	}
 
-	
-	
-	
 	
 }
